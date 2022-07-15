505ec2c419974e5d1ee4c11827a152bab0da356e
Monini1706 <64771796+EdoardoOlivato876377@users.noreply.github.com>
2022-07-16 01:16:40 +0200

    16/07

---
+++ nuovoSpazioLavoro/src/MainMAtrice.java
@@ -0,0 +1,6 @@
+public class MainMAtrice {
+    public static void main(String [] argv){
+        Matrice mati=new Matrice();
+
+    }
+}
---
+++ nuovoSpazioLavoro/src/Matrice.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+//classe Matrice
+public class Matrice {
+    //costruttore
+    public Matrice(){
+        ArrayList <ArrayList <Integer>> mat=new ArrayList<>();
+    }0.
+
+}
